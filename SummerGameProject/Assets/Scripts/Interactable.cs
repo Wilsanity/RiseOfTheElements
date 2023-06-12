df784cb9414644f23cf4893a15fe74c9a533ebd7
ShockWaveGamer <daniel.fiuk21@gmail.com>
2023-06-12 13:29:55 -0600

    Updated Interactibles to create a trigger for objects and use the new inputsystem

--- SummerGameProject/Assets/Scripts/Interactable.cs
+++ SummerGameProject/Assets/Scripts/Interactable.cs
@@ -1,35 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.InputSystem;
 
 public class Interactable : MonoBehaviour
 {
+    InputAction interact;
+    bool isInRange;
 
-    
-    public float radius = 3f;
-
-    public bool isInRange;
-    public KeyCode interactKey;
-    public UnityEvent interactAction;
-    public bool destroyOnInteract;
+    [SerializeField] float radius = 3f;
+    [SerializeField] UnityEvent interactAction;
+    [SerializeField] bool destroyOnInteract;
 
     void OnDrawGizmosSelected()
     {
+        //Draw Yellow Wire Sphere to show the range of the interactable
         Gizmos.color = Color.yellow;
-
         Gizmos.DrawWireSphere(transform.position, radius);
     }
 
-
-    private void OnTriggerStay(Collider collision)
+    private void Awake()
     {
-        if (Input.GetKeyDown(interactKey))
-        {
-            interactAction.Invoke();
+        //Set up a Sphere Trigger Collider for interaction
+        SphereCollider collider = gameObject.AddComponent<SphereCollider>();
+        collider.radius = radius;
+        collider.isTrigger = true;
+    }
 
-            if (destroyOnInteract)
-                Destroy(gameObject);
+    private void OnTriggerEnter(Collider other)
+    {
+        //If the player enters the trigger, ready the input for the interaction
+        if (other.CompareTag("Player"))
+        {
+            isInRange = true;
+
+            //Get the input action for the interaction
+            interact = other.GetComponent<PlayerInput>().actions["Interact"];
+            interact.Enable();
+
+            //This line will trigger the interaction when the input is pressed
+            interact.performed += ctx => Interact();
+        }
+    }
+
+    private void Interact()
+    {
+        //If the player isn't in range, quit opporation
+        if (!isInRange) return;
+
+        //Else do the interaction
+        interactAction.Invoke();
+        if (destroyOnInteract) Destroy(gameObject);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        //If player leaves trigger area, disable the interaction
+        if (other.CompareTag("Player"))
+        {
+            isInRange = false;
+            interact.Disable();
         }
     }
 }

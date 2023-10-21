6bb12daaa3af7303fa54195b243dc8d96cd90ed0
miandilan <miandilan@gmail.com>
2023-10-21 14:58:14 -0400

    Updated Guide

--- SummerGameProject/Assets/Scripts/Controllers/Guide/GuideController.cs
+++ SummerGameProject/Assets/Scripts/Controllers/Guide/GuideController.cs
@@ -8,12 +8,29 @@ public class GuideController : MonoBehaviour
     public float orbitSpeed = 15f; // The normal speed of the orbit
     public float boostedOrbitSpeed = 45f; // The boosted speed of the orbit
     public float speedChangeDelay = 5f; // The time in seconds between speed changes
+    public float maxRadius = 3f;
 
     private bool isBoosted = false;
     private float timer = 0f;
 
     void Update()
     {
+        
+
+        Vector3 centerToGuide = transform.position - orbitCenter.position;
+        float currentDist = centerToGuide.magnitude;
+
+        //If the guide moves beyond the maxRadius we get a new position within
+        //the maxRadius to keep it within the allowed distance from the orbit center.
+        if (currentDist > maxRadius)
+        {
+            //Get a position within the maxRadius
+            Vector3 targetPosition = orbitCenter.position + centerToGuide.normalized * maxRadius;
+            transform.position = targetPosition;
+        }
+
+        transform.LookAt(orbitCenter);//Always look at the player in a fixed rotation pattern.
+
         transform.RotateAround(orbitCenter.position, Vector3.up, orbitSpeed * Time.deltaTime);
 
         timer += Time.deltaTime;

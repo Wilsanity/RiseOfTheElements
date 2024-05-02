09e3efbce246bb20dacc187b74d7b1aab7be3c76
Lucas Z <Lucas.Zakaria03@gmail.com>
2024-05-02 01:32:11 -0400

    Made Custom Health Script
    This health script can be placed on any object, enemy, or player that requires health. There's a neat custom inspector I made to display the health in a visual form live. I also made functions to deal damage and heal health, along with setting/getting the current phase of the unit.
    This is a universal way to make health much easier to implement instead of hard coding health for each enemy. This is all visual.

---
+++ SummerGameProject/Assets/Scripts/Enemies/Editor/UnitHealthEditor.cs
@@ -0,0 +1,138 @@
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(UnitHealth))]
+public class UnitHealthEditor : Editor
+{
+    SerializedProperty _maxHealth;
+    SerializedProperty _currentHealth;
+    SerializedProperty _currentPhase;
+    SerializedProperty _unitHealthPhases;
+    SerializedProperty phaseName;
+    SerializedProperty phaseHealthPercent;
+
+    readonly Color c = Color.green;
+
+    private void OnEnable()
+    {
+        _maxHealth = serializedObject.FindProperty("_maxHealth");
+        _currentHealth = serializedObject.FindProperty("_currentHealth");
+        _currentPhase = serializedObject.FindProperty("_currentPhase");
+        _unitHealthPhases = serializedObject.FindProperty("_unitHealthPhases");
+     
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        //Text Styles
+        GUIStyle textStyle = new GUIStyle();
+        textStyle.fontSize = 16;
+        textStyle.normal.textColor = Color.white;
+
+        GUIStyle titleStyle = new GUIStyle();
+        titleStyle.fontSize = 16;
+        titleStyle.fontStyle = FontStyle.Bold;
+        titleStyle.normal.textColor = Color.white;
+        titleStyle.alignment = TextAnchor.MiddleCenter;
+
+        EditorGUILayout.PropertyField(_maxHealth);
+
+        if (!Application.isPlaying)
+        {
+            _currentHealth.intValue = _maxHealth.intValue;
+        }
+
+        //EditorGUILayout.PropertyField(_currentHealth);
+        //Current Health
+        EditorGUILayout.BeginHorizontal();
+        GUILayout.Label("Current Health ");
+        GUILayout.Label(_currentHealth.intValue.ToString(), GUILayout.MaxWidth(Screen.width / 1.8f));
+        EditorGUILayout.EndHorizontal();
+
+        //Current Phase
+        EditorGUILayout.BeginHorizontal();
+        GUILayout.Label("Current Phase");
+        GUILayout.Label(_currentPhase.intValue.ToString(), GUILayout.MaxWidth(Screen.width / 1.8f));
+        EditorGUILayout.EndHorizontal();
+
+
+
+
+        EditorGUILayout.PropertyField(_unitHealthPhases);
+
+        GUILayout.Label("Unit Health Bar", titleStyle);
+
+        //Draw the Health bar (Behind)
+        Rect healthPhasesArrayRect = new Rect(GUILayoutUtility.GetLastRect().x, GUILayoutUtility.GetLastRect().y, GUILayoutUtility.GetLastRect().width, GUILayoutUtility.GetLastRect().height);
+        EditorGUI.DrawRect(new Rect(20, healthPhasesArrayRect.y + healthPhasesArrayRect.height + 40, healthPhasesArrayRect.width - 20, 30), Color.black);
+       
+        //Draw the Current health health bar
+        EditorGUI.DrawRect(new Rect(20, 
+            healthPhasesArrayRect.y + healthPhasesArrayRect.height + 40, 
+            (healthPhasesArrayRect.width - 20) / ((float)_maxHealth.intValue / (float)_currentHealth.intValue), 
+            30), c);
+        
+        float widthOfHealthBar = healthPhasesArrayRect.width - 20;
+        GUILayout.Space(150);
+
+
+        //Draw the Min Vale and Max Value and the white rectangles at the end points
+        Rect healthBarMinValueRect = new Rect(20, healthPhasesArrayRect.y + (healthPhasesArrayRect.height + 70), 10, 50);
+        Rect healthBarMaxValueRect = new Rect(healthPhasesArrayRect.width - 5, healthPhasesArrayRect.y + (healthPhasesArrayRect.height + 70), 50, 50);
+        EditorGUI.LabelField(healthBarMinValueRect, "0");
+        EditorGUI.LabelField(healthBarMaxValueRect, _maxHealth.intValue.ToString());
+        EditorGUI.DrawRect(new Rect(healthBarMinValueRect.x, healthBarMinValueRect.y + 5, 2, -40), Color.white);
+        EditorGUI.DrawRect(new Rect(healthPhasesArrayRect.width, healthBarMaxValueRect.y + 5, 2, -40), Color.white);
+        
+        
+        
+        
+
+
+        // This is in charge of drawing the boss health increment bars based on the the healthPhases array size and data
+        //I have to put this code after we apply the modified properties or else we get an error since the array of the property doesn't update on time
+        for (int i = 0; i < _unitHealthPhases.arraySize; i++)
+        {
+            
+            phaseHealthPercent = _unitHealthPhases.GetArrayElementAtIndex(i).FindPropertyRelative("phaseHealthPercent");
+            
+
+            //Change the name of the phase in the inspector array
+            phaseName = _unitHealthPhases.GetArrayElementAtIndex(i).FindPropertyRelative("phaseName");
+
+            phaseName.stringValue = $"Phase {i + 1}";
+            //Loop through each health percent and draw a rect
+
+            float f = phaseHealthPercent.floatValue;
+
+            Rect incrementBar = new Rect((f * (widthOfHealthBar / 100)) + 20, healthPhasesArrayRect.y + healthPhasesArrayRect.height + 30, 8, 50);
+
+            //Drawing the Ticks for the health percentages
+            EditorGUI.DrawRect(incrementBar, new Color(c.r - 0.3f, c.g - 0.3f, c.b - 0.3f));
+
+
+            //Drawing the Number text for the health percentages
+            EditorGUI.LabelField(new Rect(incrementBar.x - 10, incrementBar.y + 52, 10, 50), $"{f.ToString("00")}%", textStyle);
+
+            //Drawing the phase number above the rectangle tick
+            EditorGUI.LabelField(new Rect(incrementBar.x - 25, incrementBar.y - 20, 10, 50), $"Phase {i + 1}", textStyle);
+
+
+            //if (boss.B_BossPhases[i].phaseEvent != null)
+            //{
+            //    textStyle.normal.textColor = new Color(0.2f, 0.69f, 0.92f, 1);
+
+            //    //Drawing the Event text for the phase
+            //    EditorGUI.LabelField(new Rect(incrementBar.x - 15, incrementBar.y + 66, 10, 50), "Event", textStyle);
+            //}
+
+
+            
+        }
+        serializedObject.ApplyModifiedProperties();
+
+    }
+
+}
---
+++ SummerGameProject/Assets/Scripts/Enemies/UnitHealth.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitHealth : MonoBehaviour
+{
+    //Stores the max health, current health, and phases if the enemy has.
+
+    //Fields
+    [SerializeField] private int _maxHealth;
+    [SerializeField] private int _currentHealth;
+    [SerializeField] private int _currentPhase = 1;
+
+    [SerializeField] private UnitHealthPhases[] _unitHealthPhases = new UnitHealthPhases[0];
+
+    //Properties
+    public int CurrentHealth { get => _currentHealth; set => _currentHealth = value; }
+    public int MaxHealth { get => _maxHealth;}
+    public UnitHealthPhases[] UnitHealthPhases { get => _unitHealthPhases; set => _unitHealthPhases = value; }
+    public int CurrentPhase { get => _currentPhase;}
+
+
+
+    //Deal Damage to the unit this script is attached to
+    public void DamageUnit(int damageAmount)
+    {
+        if (_currentHealth <= 0) return;
+
+        //reduce health and make sure we can't go into the negatives
+        _currentHealth = Mathf.Clamp(_currentHealth - damageAmount, 0, _maxHealth);
+
+        if (_currentHealth <= 0)
+        {
+            //Destroy Unit
+            Destroy(gameObject);
+        }
+
+        if (_unitHealthPhases.Length != 0) _currentPhase = SetCurrentHealthPhase();
+    }
+
+
+    //Heal the unit this script is attached to
+    public void HealUnit(int healAmount)
+    {
+        if (_currentHealth >= _maxHealth) return;
+
+        //heal the unit and make sure their health can't go over their maxHealth
+        _currentHealth = Mathf.Clamp(_currentHealth + healAmount, 0, _maxHealth);
+
+        if (_unitHealthPhases.Length != 0) _currentPhase = SetCurrentHealthPhase();
+    }
+
+
+    //Mainly for bosses, get the current phase the unit is at if it has any
+    private int SetCurrentHealthPhase()
+    {
+        
+        //Get percent health from max health
+        float healthPercent = (_currentHealth / _maxHealth * 100);
+
+        //loop through the phases
+
+        //if lower than one, set that phase
+
+        //For loop to see if it is > the current increment or not
+        for (int i = _unitHealthPhases.Length - 1; i >= 0; i--)
+        {
+
+            //eg. if the current health is 65%, then we want to check if <25, then <50, then <75, then <100
+
+            if (healthPercent > _unitHealthPhases[i].phaseHealthPercent)
+                continue;
+
+
+            //Play the special Health Increment event if this phase has one and it hasn't already played
+            //if (_unitHealthPhases[i].phaseEvent != null)
+            //{
+            //    if (!bossProfile.B_BossPhases[i].phaseEvent.EventPlayed)
+            //        bossProfile.B_BossPhases[i].phaseEvent.OnHealthChange();
+            //}
+
+            return i + 1;
+        }
+
+        return 0;
+    }
+}
+
+[System.Serializable]
+public class UnitHealthPhases
+{
+    [HideInInspector] public string phaseName = "Phase ";
+
+    [Range(0, 100)]
+    public float phaseHealthPercent = 55;
+
+}

ffdbdafb17e23abae90ab359827686bbacb81adc
Pedro Zorrilla <tmm.pedro.zorrilla@gmail.com>
2024-06-06 14:15:04 -0700

    Reapplied prefab values to nested prefabs. Cleaned up controller script

--- SummerGameProject/Assets/Scripts/Controllers/Player Controller/PlayerController.cs
+++ SummerGameProject/Assets/Scripts/Controllers/Player Controller/PlayerController.cs
@@ -39,9 +39,9 @@ public class PlayerController : MonoBehaviour
 
     #region inspector
 
-    [SerializeField] float moveSpeed;
-    [SerializeField] float jumpPower;
-    [SerializeField] float sprintPower;
+    //[SerializeField] float moveSpeed;
+    //[SerializeField] float jumpPower;
+    //[SerializeField] float sprintPower;
     [SerializeField] Image healthBar;
 
 
@@ -214,105 +214,8 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    //This is when the player attacks the cave plant enemies. This is a temporary solution since using an array caused them collectively to die
-    //when only 1 was killed by the player.
-    //private void OnTriggerEnter(Collider other)
-    //{
-        
-    //   if(other.gameObject.CompareTag("DamageZone") && attackAction.ReadValue<float>() != 0)
-    //    {
-    //        PlantAIController plantAI = plantEnemy.GetComponent<PlantAIController>();
-    //        if (plantAI != null)
-    //        {
-    //            plantAI.TakeDamage();
-    //            Debug.Log("Plant1 Health: " + plantAI.health);
-    //        }
-    //    }
+    
     //}
-
-    //private void Move()
-    //{
-
-    //    //Reads player input as a vector2
-    //    Vector2 moveInput = moveAction.ReadValue<Vector2>();
-    //    if (sprintAction.ReadValue<float>() != 0) moveInput *= sprintPower;
-
-    //    /*
-    //    Because we want the player to move at a consistant speed regardless of the angle of the ground they're walking on.
-    //    We find the ground normal and find the cross product for the forward and right vectors.
-    //    We then use the cross product to find the direction the player should move in, and we apply our input to that direction.
-    //    */
-
-    //    //----------------------old non-navmesh code-------------------------------------------------------------
-    //    //Find the new forward and right vectors
-    //    //Vector3 forward = Vector3.Cross(GroundedNormal, cameraFollowTargetTransform.right);
-    //    //Vector3 right = Vector3.Cross(GroundedNormal, cameraFollowTargetTransform.forward);
-
-    //    ////Apply the input to the new forward and right vectors and use those values as the Rigidbodies velocity
-    //    //Vector3 moveDirection = forward * -moveInput.y + right * moveInput.x;
-    //    //----------------------old non-navmesh code--------------------------------------------------------------
-
-
-    //    Vector3 moveDirection = new Vector3(moveInput.x, 0f, moveInput.y);
-    //    moveDirection = cameraFollowTargetTransform.TransformDirection(moveDirection);
-    //    moveDirection.y = 0f;
-
-    //    //Ensures that the NavMeshAgent is enabled before setting its destination.
-    //    //Set the NavMeshAgent destination using nma.SetDestination.
-    //    if (nma.enabled) nma.SetDestination(transform.position + moveDirection);
-
-    //    //Rotate the player to face forward
-    //    Quaternion targetRotation = moveDirection != Vector3.zero ? Quaternion.LookRotation(moveDirection, Vector3.up) : transform.rotation;
-        
-    //    if (moveInput.magnitude >= 0.3)
-    //    {
-    //        PlayerAnimationMachine.UpdatePlayerAnim(PlayerAnimState.IsMoving, true, anim);
-    //        PlayerAnimationMachine.UpdatePlayerAnim(PlayerAnimState.IsSprinting, sprintAction.ReadValue<float>() != 0, anim);
-    //        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * 10f);
-    //    }
-    //    else
-    //    {
-    //        PlayerAnimationMachine.UpdatePlayerAnim(PlayerAnimState.IsMoving, false, anim);
-    //    }
-
-    //    if (isGrounded) body.velocity = Vector3.Lerp(body.velocity, moveDirection * moveSpeed, Time.deltaTime * 6f);
-    //    else body.velocity = Vector3.Lerp(body.velocity, moveDirection * moveSpeed, Time.deltaTime * 1f);
-    //}
-
-    private IEnumerator Jump()
-    {
-        //if player is either not grounded or the jump is still on a cool down, stop the coroutine.
-        if (!isGrounded || jumpOnCoolDown) yield break;
-
-        jumpOnCoolDown = true;
-
-        isGrounded = false;
-
-        //Stop navmeshagent to ensure a controlled jump.
-        nma.velocity = Vector3.zero;
-
-        //Calculate the jump direction based on current ground normal.
-        Vector3 jumpDirection = GroundedNormal + Vector3.up;
-
-        //Set nma's velocity for a jump;
-        nma.velocity = jumpDirection * jumpPower;
-
-        //----------------------old non-navmesh code--------------------------------------------------------------
-        //Vector3 vertical = new Vector3(0.0f, body.velocity.y, 0.0f);
-        //Vector3 horizontal = new Vector3(body.velocity.x, 0.0f, body.velocity.z);
-        //body.velocity = (horizontal + (vertical * 0.1f));
-        //body.AddForce(horizontal * 10, ForceMode.Force); //Jumping while moving gives a slight boost in your current direction.
-        //body.AddForce(GroundedNormal * jumpPower * 75, ForceMode.Force); //Pushes off the ground, using the normal of the collision surface.
-        //body.AddForce(Vector3.up * jumpPower * 25, ForceMode.Force);
-        //----------------------old non-navmesh code--------------------------------------------------------------
-
-        //Wait for the jump to cool down
-        yield return new WaitForSeconds(0.1f);
-
-        jumpOnCoolDown = false;
-
-       
-    }
     public void Attack()
     {
         //if not ready to attack or is attacking, return
@@ -387,5 +290,105 @@ public class PlayerController : MonoBehaviour
         }
         
     }
+
+    // legacy code
+    //This is when the player attacks the cave plant enemies. This is a temporary solution since using an array caused them collectively to die
+    //when only 1 was killed by the player.
+    //private void OnTriggerEnter(Collider other)
+    //{
+
+    //   if(other.gameObject.CompareTag("DamageZone") && attackAction.ReadValue<float>() != 0)
+    //    {
+    //        PlantAIController plantAI = plantEnemy.GetComponent<PlantAIController>();
+    //        if (plantAI != null)
+    //        {
+    //            plantAI.TakeDamage();
+    //            Debug.Log("Plant1 Health: " + plantAI.health);
+    //        }
+    //    }
+    //}
+
+    //private void Move()
+    //{
+
+    //    //Reads player input as a vector2
+    //    Vector2 moveInput = moveAction.ReadValue<Vector2>();
+    //    if (sprintAction.ReadValue<float>() != 0) moveInput *= sprintPower;
+
+    //    /*
+    //    Because we want the player to move at a consistant speed regardless of the angle of the ground they're walking on.
+    //    We find the ground normal and find the cross product for the forward and right vectors.
+    //    We then use the cross product to find the direction the player should move in, and we apply our input to that direction.
+    //    */
+
+    //    //----------------------old non-navmesh code-------------------------------------------------------------
+    //    //Find the new forward and right vectors
+    //    //Vector3 forward = Vector3.Cross(GroundedNormal, cameraFollowTargetTransform.right);
+    //    //Vector3 right = Vector3.Cross(GroundedNormal, cameraFollowTargetTransform.forward);
+
+    //    ////Apply the input to the new forward and right vectors and use those values as the Rigidbodies velocity
+    //    //Vector3 moveDirection = forward * -moveInput.y + right * moveInput.x;
+    //    //----------------------old non-navmesh code--------------------------------------------------------------
+
+
+    //    Vector3 moveDirection = new Vector3(moveInput.x, 0f, moveInput.y);
+    //    moveDirection = cameraFollowTargetTransform.TransformDirection(moveDirection);
+    //    moveDirection.y = 0f;
+
+    //    //Ensures that the NavMeshAgent is enabled before setting its destination.
+    //    //Set the NavMeshAgent destination using nma.SetDestination.
+    //    if (nma.enabled) nma.SetDestination(transform.position + moveDirection);
+
+    //    //Rotate the player to face forward
+    //    Quaternion targetRotation = moveDirection != Vector3.zero ? Quaternion.LookRotation(moveDirection, Vector3.up) : transform.rotation;
+
+    //    if (moveInput.magnitude >= 0.3)
+    //    {
+    //        PlayerAnimationMachine.UpdatePlayerAnim(PlayerAnimState.IsMoving, true, anim);
+    //        PlayerAnimationMachine.UpdatePlayerAnim(PlayerAnimState.IsSprinting, sprintAction.ReadValue<float>() != 0, anim);
+    //        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * 10f);
+    //    }
+    //    else
+    //    {
+    //        PlayerAnimationMachine.UpdatePlayerAnim(PlayerAnimState.IsMoving, false, anim);
+    //    }
+
+    //    if (isGrounded) body.velocity = Vector3.Lerp(body.velocity, moveDirection * moveSpeed, Time.deltaTime * 6f);
+    //    else body.velocity = Vector3.Lerp(body.velocity, moveDirection * moveSpeed, Time.deltaTime * 1f);
+    //}
+
+    //private IEnumerator Jump()
+    //{
+    //    //if player is either not grounded or the jump is still on a cool down, stop the coroutine.
+    //    if (!isGrounded || jumpOnCoolDown) yield break;
+
+    //    jumpOnCoolDown = true;
+
+    //    isGrounded = false;
+
+    //    //Stop navmeshagent to ensure a controlled jump.
+    //    nma.velocity = Vector3.zero;
+
+    //    //Calculate the jump direction based on current ground normal.
+    //    Vector3 jumpDirection = GroundedNormal + Vector3.up;
+
+    //    //Set nma's velocity for a jump;
+    //    nma.velocity = jumpDirection * jumpPower;
+
+    //    //----------------------old non-navmesh code--------------------------------------------------------------
+    //    //Vector3 vertical = new Vector3(0.0f, body.velocity.y, 0.0f);
+    //    //Vector3 horizontal = new Vector3(body.velocity.x, 0.0f, body.velocity.z);
+    //    //body.velocity = (horizontal + (vertical * 0.1f));
+    //    //body.AddForce(horizontal * 10, ForceMode.Force); //Jumping while moving gives a slight boost in your current direction.
+    //    //body.AddForce(GroundedNormal * jumpPower * 75, ForceMode.Force); //Pushes off the ground, using the normal of the collision surface.
+    //    //body.AddForce(Vector3.up * jumpPower * 25, ForceMode.Force);
+    //    //----------------------old non-navmesh code--------------------------------------------------------------
+
+    //    //Wait for the jump to cool down
+    //    yield return new WaitForSeconds(0.1f);
+
+    //    jumpOnCoolDown = false;
+
+
 
 }
